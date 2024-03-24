2addf7551a58a223f72096df895b7b26554212fc
Kafui Mortey <morteykafui@gmail.com>
2024-03-24 21:27:58 +0000

    add user authentication

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,27 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { ModeToggle } from "./ToggleModeButton";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
-const Navbar = () => {
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+const Navbar = async () => {
+  const { isAuthenticated, getUser } = getKindeServerSession();
+  const user = await getUser();
+
   return (
     <nav className="border-b-2 border-rose-500 sticky w-full top-0 dark:bg-[#0c0a09] bg-white">
       <div className="max-w-screen-xl mx-auto px10 py-4 container flex justify-between items-center text-lg font-medium">
@@ -11,39 +30,79 @@ const Navbar = () => {
             Doc<span className="text-rose-500">Flow</span>
           </Link>
         </h1>
-        <ul className="flex gap-8">
-          <li>
-            <Link
-              href="/all-projects"
-              className="hover:text-rose-700 transition-all duration-500"
-            >
-              Projects
-            </Link>
-          </li>
+        {(await isAuthenticated()) ? (
+          <ul className="flex gap-8">
+            <li>
+              <Link
+                href="/all-projects"
+                className="hover:text-rose-700 transition-all duration-500"
+              >
+                Projects
+              </Link>
+            </li>
 
-          <li>
+            {/* <li>
             <Link
               href="/about"
               className="hover:text-rose-700 transition-all duration-500"
             >
               About
             </Link>
-          </li>
-          <li>
-            <Link
-              href="/add"
-              className="hover:text-rose-700 transition-all duration-500"
-            >
-              Add New Project
-            </Link>
-          </li>
-        </ul>
+          </li> */}
+            <li>
+              <Link
+                href="/add"
+                className="hover:text-rose-700 transition-all duration-500"
+              >
+                Add New Project
+              </Link>
+            </li>
+          </ul>
+        ) : null}
         <div className="flex gap-4 items-center">
-          <Button asChild>
+          {!(await isAuthenticated()) ? (
+            <>
+              <Button asChild>
+                <RegisterLink>Sign up</RegisterLink>
+              </Button>
+              <Button variant="ghost" asChild>
+                <LoginLink>Sign in</LoginLink>
+              </Button>
+            </>
+          ) : (
+            <>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Avatar>
+                    <AvatarImage src={user?.picture as string} />
+                    <AvatarFallback>?</AvatarFallback>
+                  </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>{user?.given_name}</DropdownMenuItem>
+                  <DropdownMenuItem>{user?.family_name}</DropdownMenuItem>
+                  <DropdownMenuItem>{user?.email}</DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Button
+                      size="sm"
+                      variant="ghost"
+                      className="w-full"
+                      asChild
+                    >
+                      <LogoutLink>Log out</LogoutLink>
+                    </Button>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </>
+          )}
+          {/* <Button asChild>
             <Link href="/login" className="font-bold uppercase text-lg">
               Login
             </Link>
-          </Button>
+          </Button> */}
           <ModeToggle />
         </div>
       </div>
